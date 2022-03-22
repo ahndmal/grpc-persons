bc97cd36eb0a40ccff0ecf51f11fe6c30b035ad0
Andrii Maliuta <36703491+AndriiMaliuta@users.noreply.github.com>
2022-03-22 19:29:19 +0200

    init

--- src/main/java/com/anma/grpc/PersonDemo.java
+++ src/main/java/com/anma/grpc/PersonDemo.java
@@ -1,6 +1,7 @@
 package com.anma.grpc;
 
 import com.anma.grpc.models.Person;
+import com.anma.grpc.srv.PersonService;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,29 +10,19 @@ import java.nio.file.StandardOpenOption;
 
 public class PersonDemo {
     public static void main(String[] args) throws IOException {
+        // WRITE
         Person vasyl = Person.newBuilder()
                 .setAge(20)
                 .setName("Vasyl")
                 .build();
 
-        writePerson(vasyl);
+        PersonService.writePerson(vasyl);
 
-        Person two = read("models/person");
-        System.out.println(two);
+        // READ
+        Person vasyl2 = PersonService.read("models/person");
+        System.out.println(vasyl2);
 
     }
 
-    private static void writePerson(Person person) throws IOException {
-       person.writeTo(Files.newOutputStream(Path.of("models/person"), StandardOpenOption.WRITE));
-    }
 
-    public static Person read(String file) {
-        Person person = null;
-        try {
-            person = Person.parseFrom(Files.newInputStream(Path.of(file), StandardOpenOption.CREATE_NEW));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return person;
-    }
 }
--- src/main/java/com/anma/grpc/srv/PersonService.java
+++ src/main/java/com/anma/grpc/srv/PersonService.java
@@ -1,4 +1,29 @@
 package com.anma.grpc.srv;
 
+import com.anma.grpc.models.Person;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public class PersonService  {
+
+    public static void writePerson(Person person)  {
+        try {
+            person.writeTo(Files.newOutputStream(Path.of("models/person"), StandardOpenOption.WRITE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Person read(String file) {
+        Person person = null;
+        try {
+            person = Person.parseFrom(Files.newInputStream(Path.of(file), StandardOpenOption.CREATE_NEW));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return person;
+    }
 }
